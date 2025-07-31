75c5a7de260df6ed95c216572aa6bd91948a0d59
Rq4n <ryanlimabatista2@gmail.com>
2025-07-31 17:41:55 -0300

    rm transparent

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -13,40 +13,6 @@ return {
       })
     end,
   },
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = { "hrsh7th/cmp-nvim-lsp" }, -- necessário para capabilities
-    config = function()
-      local lspconfig = require("lspconfig")
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-      -- Go
-      lspconfig.gopls.setup({
-        capabilities = capabilities,
-      })
-
-      -- Lua
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-        settings = {
-          Lua = {
-            runtime = {
-              version = "LuaJIT",
-            },
-            diagnostics = {
-              globals = { "vim" },
-            },
-            workspace = {
-              library = vim.api.nvim_get_runtime_file("", true),
-              checkThirdParty = false,
-            },
-            telemetry = {
-              enable = false,
-            },
-          },
-        },
-      })
-    end,
-  },
 }
+  
 
