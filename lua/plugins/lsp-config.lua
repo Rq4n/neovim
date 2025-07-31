77041d2ede679362fba6be1236d3c9ba6cc1df0b
Rq4n <ryanlimabatista2@gmail.com>
2025-07-31 19:07:57 -0300

    feat: vim diagnostics

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -3,7 +3,7 @@ return {
     "williamboman/mason.nvim",
     config = function()
       require("mason").setup()
-    end,
+    end
   },
   {
     "williamboman/mason-lspconfig.nvim",
@@ -11,8 +11,33 @@ return {
       require("mason-lspconfig").setup({
         ensure_installed = { "lua_ls", "gopls" },
       })
-    end,
+    end
   },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      lspconfig.gopls.setup({})
+
+      local opts = { noremap = true, silent = true }
+
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+      vim.keymap.set('n', "<leader>gd", vim.lsp.buf.definition, opts)
+      vim.keymap.set({'n'}, '<leader>ca', vim.lsp.buf.code_action, opts)
+
+      -- diagnostics
+      vim.diagnostic.config({
+        virtual_text = true,
+        signs = true,
+        underline = true,
+        update_in_insert = false,
+        severity_sort = true,
+      })
+    end
+  }
 }
-  
+
+
+
 
