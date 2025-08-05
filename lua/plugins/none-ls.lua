d1ec0b7d6e670de1114e56abcb7aec78588025e3
Rq4n <ryanlimabatista2@gmail.com>
2025-08-05 12:50:28 -0300

    feat: cmp

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,22 +1,21 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        -- lua
-        null_ls.builtins.formatting.stylua,
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				-- lua
+				null_ls.builtins.formatting.stylua,
 
-        -- javascripto
-        -- null_ls.builtins.formatting.prettier,
-        -- null_ls.builtins.diagnostics.eslint_d,`
+				-- javascripto
+				-- null_ls.builtins.formatting.prettier,
+				-- null_ls.builtins.diagnostics.eslint_d,`
 
-
-        -- golang
-        null_ls.builtins.diagnostics.golangci_lint,
-        null_ls.builtins.formatting.goimports,
-      },
-    })
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+				-- golang
+				null_ls.builtins.diagnostics.golangci_lint,
+				null_ls.builtins.formatting.goimports,
+			},
+		})
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
 }
