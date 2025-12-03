56c506c0cfdb30dbf8101e62e42e30519bd80824
Rq4n <ryanlimabatista2@gmail.com>
2025-12-03 11:19:09 -0300

    fixed keybinds and testing copilot.nvim

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,6 +1,12 @@
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
+
+-- split window
 keymap.set("n", "ss", ":split<Return>", opts)
 keymap.set("n", "sv", ":vsplit<Return>", opts)
--- Move window
+-- move window
+keymap.set("n", "sh", "<C-w>h")
+keymap.set("n", "sk", "<C-w>k")
+keymap.set("n", "sj", "<C-w>j")
+keymap.set("n", "sl", "<C-w>l")
