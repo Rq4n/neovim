78790950d644a7dfdb4d452440c04269caf980f0
Rq4n <ryanlimabatista2@gmail.com>
2025-12-03 23:43:35 -0300

    add tmux navigations

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,15 +1,29 @@
 return {
   "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
   config = function()
     local null_ls = require("null-ls")
+
     null_ls.setup({
       sources = {
+        -- Lua
         null_ls.builtins.formatting.stylua,
+
+        -- Golang
         null_ls.builtins.diagnostics.golangci_lint,
         null_ls.builtins.formatting.goimports,
+
+        -- Python
         null_ls.builtins.formatting.black,
+
+        -- Javascript / Typescript / React / HTML / JSON / CSS
+        null_ls.builtins.formatting.prettier,
       },
     })
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+
+    -- Format on save (optional but recommended)
+    vim.keymap.set("n", "gf", vim.lsp.buf.format, { desc = "Format buffer" })
   end,
 }
